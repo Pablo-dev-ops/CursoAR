ad5947710d2dcdf54ecaae3eb714bd4c5e57c3cf
pablo-dev-ops <pframoy319@g.educaand.es>
2024-11-05 11:52:20 +0100

    Primer commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public event Action OnMainMenu;
+    public event Action OnItemsMenu;
+    public event Action OnARPosition;
+
+    public static GameManager instance;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+
+        if (instance == null && instance != this) Destroy(gameObject);
+        else instance = this;
+
+    }
+    void Start()
+    {
+        MainMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MainMenu()
+    {
+        OnMainMenu?.Invoke();
+        Debug.Log("Main Menu Activated");
+    }
+    public void ItemsMenu()
+    {
+        OnItemsMenu?.Invoke();
+        Debug.Log(" Items Menu Activated");
+    }
+    public void ARPosition() 
+    {
+        OnARPosition?.Invoke();
+        Debug.Log("AR Position Activated");
+    }
+    public void CloseAPP()
+    {
+        Application.Quit();
+    }
+}
