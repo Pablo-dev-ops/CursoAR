a1baa0aaa10349dec8e54e47fb8e32211d470ab9
pablo-dev-ops <pframoy319@g.educaand.es>
2024-11-06 12:47:46 +0100

    Menus implementado

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,7 +14,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
 
-        if (instance == null && instance != this) Destroy(gameObject);
+        if (instance != null && instance != this) Destroy(gameObject);
         else instance = this;
 
     }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    
+    public string itemName;
+    public Sprite itemImage;
+    public string itemDescription;
+    public GameObject item3DModel;
+
+
+
+}
